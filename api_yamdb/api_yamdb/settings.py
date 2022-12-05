f51882f37f04604fd792ec0fa357cc1b08a5879c
Danil Raspopov <anarant91@gmail.com>
2022-12-05 17:03:46 +0300

    update

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -31,6 +31,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api',
+    'reviews',
 ]
 
 ROOT_URLCONF = 'api_yamdb.urls'
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,6 +2,70 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+class Category(models.Model):
+    name = models.CharField('название категории', max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField('название жанра', max_length=256)
+    slug = models.SlugField(
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('название', max_length=256)
+    year = models.IntegerField(
+        'год',
+        max_length=4,
+    )
+    # добавить ограничение
+    description = models.TextField('описание', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='категория',
+        help_text='выберете категорию',
+        null=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='жанр',
+        help_text='выберете жанр',
+        null=True
+    )
+
+    class Meta:
+        ordering = ('-year',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
 class Review(models.Model):
     title = models.ForeignKey(
         Title,
