18708f32c55cca48c284b843190af53fd4bd93b8
Max Milenin <mvmilenin@gmail.com>
2022-12-05 18:09:50 +0300

    added roles ot model

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -9,3 +9,42 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = UserCustomized
+
+
+class UserSignUpSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserCustomized
+        fields = ('email', 'username')
+
+    def validate_username(self, value):
+        if (value == 'me'):
+            raise serializers.ValidationError('Недопустимое имя пользователя')
+        return value
+
+
+class TokenRequest():
+    def __init__(self, confirmation_code, username):
+        self.confirmation_code = confirmation_code
+        self.username = username
+
+
+class TokenRequestSerializer(serializers.Serializer):
+    confirmation_code = serializers.CharField(read_only=True)
+    username = serializers.SlugField(max_length=150, read_only=True)
+
+    '''class Meta:
+        fields = ('username', 'confirmation_code')
+        model = UserCustomized'''
+
+    '''def get_confirmation_code(self, obj):
+        return self.confirmation_code'''
+
+
+'''class TokenResponse():
+    def __init__(self, access_token):
+        self.access_token = access_token'''
+
+
+class TokenResponseSerializer(serializers.Serializer):
+    access_token = serializers.CharField()
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from api.views import UserViewSet
 
 app_name = 'api'
 
-router_v1 = DefaultRouter()
+router_v1 = SimpleRouter()
 
-router_v1.register('posts', UserViewSet)
+router_v1.register('users', UserViewSet)
 
 
 urlpatterns = [
