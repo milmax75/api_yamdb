677c3a6c7dc8824ee139903367ef507fbdfc4ac3
Max Milenin <mvmilenin@gmail.com>
2022-12-05 18:40:01 +0300

    User with decorators

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,14 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class UserCustomized(AbstractUser):
     '''Детальная информация о пользователе'''
-    username = models.SlugField(max_length=150,
-                                unique=True,
-                                blank=False,
-                                null=False)
-    email = models.EmailField(max_length=254, unique=True, null=False)
-    first_name = models.TextField(max_length=150)
-    last_name = models.TextField(max_length=150)
-    bio = models.CharField(max_length=500)
     USER = 'user'
     MODERATOR = 'moderator'
     ADMIN = 'admin'
@@ -20,15 +12,22 @@ class UserCustomized(AbstractUser):
         (MODERATOR, 'moderator'),
         (ADMIN, 'admin')
     ]
+    username = models.SlugField(max_length=150,
+                                unique=True,
+                                blank=False,
+                                null=False)
+    email = models.EmailField(max_length=254, unique=True, null=False)
+    first_name = models.TextField(max_length=150)
+    last_name = models.TextField(max_length=150)
+    bio = models.CharField(max_length=500)
     role = models.CharField(max_length=10,
                             choices=ROLE_CHOICES,
                             default='user')
 
+    @property
     def is_moderator(self):
         return self.role == self.MODERATOR
 
-    def is_user(self):
-        return self.role == self.USER
-
+    @property
     def is_admin(self):
         return self.role == self.ADMIN
