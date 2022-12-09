56a3d3d35a97e1d7145647b7a7465aa05b7c63d6
Max Milenin <mvmilenin@gmail.com>
2022-12-09 21:45:11 +0300

    for review 1.2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'django_filters',
     'reviews',
