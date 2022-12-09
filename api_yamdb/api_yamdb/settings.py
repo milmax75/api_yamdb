28b0ed0f92e76e9fcdbc03e3ae8617570e189769
Danil Raspopov <anarant91@gmail.com>
2022-12-09 17:00:50 +0300

    ver0.4

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -125,8 +125,6 @@ REST_FRAMEWORK = {
 
 }
 SIMPLE_JWT = {
-    # Устанавливаем срок жизни токена
-
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Category, Comment, Genre, Review, Title, UserCustomized
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class UserResource(resources.ModelResource):
+
+    class Meta:
+        model = UserCustomized
+        fields = (
+            'id',
+            'username',
+            'email',
+            'role',
+            'bio',
+            'first_name',
+            'last_name',
+        )
+
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_classes = [UserResource]
+    list_display = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'bio',
+        'role'
+    )
+    list_editable = (
+        'first_name',
+        'last_name',
+        'bio',
+        'role',
+    )
+    empty_value_display = '-пусто-'
 
 
 admin.site.register(UserCustomized, UserAdmin)
