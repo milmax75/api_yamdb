d1d9f01ab8d51edf6bd35cd143b1a3928f8960d7
D-Nevskiy <110175426+D-Nevskiy@users.noreply.github.com>
2022-12-14 14:32:27 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -16,11 +16,11 @@ class IsModerOrAdmOrAuthor(permissions.BasePermission):
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and 
-                    request.user.is_admin)
+                or (request.user.is_authenticated and
+                    request.user.is_admin))
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated and 
+        return (request.user.is_authenticated and
                 request.user.is_admin)
